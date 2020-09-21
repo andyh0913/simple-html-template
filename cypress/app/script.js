4f0144642c14b7e5178cc7b7c8a42e5884695c46
andyh0913 <andyh0913@gmail.com>
2020-09-22 01:41:25 +0800

    remove console.log in script.js

--- cypress/app/script.js
+++ cypress/app/script.js
@@ -1,7 +1,6 @@
 let add_button = document.getElementById('add-button');
 let number_div = document.getElementById('number-div');
 add_button.onclick = () => {
-    console.log("pressed");
     let num = parseInt(number_div.innerText)+1;
     number_div.innerText = num.toString()
 }
